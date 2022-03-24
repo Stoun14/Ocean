7d299572b616fa058c93bdaa127c77020f15f2fc
cedric silene <cedric.silene@laposte.net>
2022-03-24 12:52:44 +0100

    Creation des CRUD

---
+++ src/Form/OrdersType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Orders;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrdersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('total')
+            ->add('pending')
+            ->add('payed')
+            ->add('fail')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('products')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Orders::class,
+        ]);
+    }
+}
